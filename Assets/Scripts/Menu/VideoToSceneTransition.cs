5d227800e4e226dd0535ba9a789e2144d7a0bc02
Will Kuntze <99731358+arborium-dev@users.noreply.github.com>
2026-08-08 10:56:38 -0400

    cutscene opening

---
+++ Assets/Scripts/Menu/VideoToSceneTransition.cs
@@ -0,0 +1,76 @@
+// VideoToSceneTransition.cs
+// this file manages MOST of the scene transitions from scenes that are just videos. This also includes the ability to skip cutscenes
+
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoToSceneTransition : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    private VideoPlayer videoPlayer;
+    private InputAction skipCutscene;
+    private bool hasTransitioned = false;
+
+    [SerializeField] private InputActionAsset inputActions;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.isLooping = false;
+        videoPlayer.loopPointReached += OnVideoEnd;
+
+        skipCutscene = inputActions != null ? inputActions.FindAction("Skip", false) : null;
+}
+
+    void OnEnable()
+    {
+        skipCutscene?.Enable();
+    }
+
+    void OnDisable()
+    {
+        skipCutscene?.Disable();
+    }
+
+    void Update()
+    {
+        if (hasTransitioned)
+            return;
+
+        if (skipCutscene != null && skipCutscene.WasPressedThisFrame())
+        {
+            OnVideoEnd(videoPlayer);
+        }
+    }
+
+    private void OnVideoEnd(VideoPlayer vp)
+    {
+        if (hasTransitioned)
+            return;
+
+        hasTransitioned = true;
+
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.LogWarning("VideoToSceneTransition OnVideoEnd() called without sceneToLoad");
+            hasTransitioned = false;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (videoPlayer != null)
+        {
+            videoPlayer.loopPointReached -= OnVideoEnd;
+        }
+    }
+}
